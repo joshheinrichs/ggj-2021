3071be41d7de91d4d9b5c97635fd985df08ee690
Josh Heinrichs <joshiheinrichs@gmail.com>
2021-01-30 13:10:22 -0600

    Add Bug
    Sorry, this bug cannot be fixed. If you try to climb the bug the game
    will crash. Avoiding touching Player.gd since Tom is working on it at
    the moment.

---
+++ Bug.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+const MAX_DISTANCE = 100
+const MAX_VELOCITY = 10
+
+var origin
+var velocity = Vector2.ZERO
+
+func _ready():
+	origin = position
+
+func _physics_process(delta):
+	# move like a bug
+	var jitter = Vector2(rand_range(-1, 1), rand_range(-1, 1))
+	# bias movement towards the origin
+	var bias = (origin - position) / MAX_DISTANCE
+	var acceleration = bias + jitter
+	velocity += acceleration
+	# cap velocity
+	if velocity.x > MAX_VELOCITY or velocity.y > MAX_VELOCITY:
+		velocity = velocity.normalized() * MAX_VELOCITY
+	position += velocity * delta
