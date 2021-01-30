d0442a317601a52f142fe29b7257a443a76415ad
HelmutShaftshwingen <alexander.n.heilmann@gmail.com>
2021-01-30 08:51:15 -0600

    start of saturday
    added  broken state machine

--- Player.gd
+++ Player.gd
@@ -1,5 +1,11 @@
 extends KinematicBody2D
 
+enum{
+	IDLE
+	RUNNING
+	CLIMB_TREE
+}
+
 const UP = Vector2(0,-1)
 const GRAVITY = 20
 const JUMP = -600
@@ -9,50 +15,51 @@ const DECCEL = 0.9
 var move_vec = Vector2()
 var leftright = 0
 var treeInRange = false
+var state = RUNNING
 
 onready var Overlap = $Overlap_Area
 
-
-func _physics_process(delta):
-		
-	if treeInRange == true:	
-		if Input.is_action_pressed("move_up"):
-			move_vec.y -= 1 * MOVE_SPEED
-		if Input.is_action_pressed("move_down"):
-			move_vec.y += 1 * MOVE_SPEED
-
-	if Input.is_action_pressed("move_right") and is_on_floor():
-		move_vec.x += 1	* MOVE_SPEED
-	elif Input.is_action_pressed("move_left") and is_on_floor():
-		move_vec.x -= 1	* MOVE_SPEED
-	else:
-		if is_on_floor():
-			move_vec.x *= DECCEL
-	
-	
-
-	#move_vec = move_vec.normalized() * MOVE_SPEED 
-	if treeInRange == false:
-		move_vec.y += GRAVITY
-	
-	move_vec = move_and_slide(move_vec, UP)
-	
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		move_vec.y = JUMP
-	
-	
-	
-	
-#	var look_vec = get_global_mouse_position() - global_position
-#	global_rotation = atan2(look_vec.y, look_vec.x)
-
-
 func _on_Overlap_Area_area_entered(area):
 	print("It's " + area.name)
-	if area.name == "Tree":
-		treeInRange = true
-
+	if area.name == "Tree" and (Input.is_action_just_pressed("move_up") or Input.is_action_just_pressed("move_up")):
+		state = CLIMB_TREE
+		print(state)
 
 func _on_Overlap_Area_area_exited(area):
 	if area.name == "Tree":
 		treeInRange = false
+		#state = RUNNING
+
+func _physics_process(delta):
+	match state:
+		IDLE:
+			pass
+		RUNNING:
+			if Input.is_action_pressed("move_right") and is_on_floor():
+				move_vec.x += 1	* MOVE_SPEED
+			elif Input.is_action_pressed("move_left") and is_on_floor():
+				move_vec.x -= 1	* MOVE_SPEED
+			else:
+				if is_on_floor():
+					move_vec.x *= DECCEL
+			
+			#move_vec = move_vec.normalized() * MOVE_SPEED 
+			if treeInRange == false:
+				move_vec.y += GRAVITY
+			
+			move_vec = move_and_slide(move_vec, UP)
+			
+			if is_on_floor() and Input.is_action_just_pressed("jump"):
+				move_vec.y = JUMP
+
+		CLIMB_TREE:
+			move_vec = Vector2.ZERO
+			if Input.is_action_pressed("move_up"):
+				move_vec.y = -1 * MOVE_SPEED
+			if Input.is_action_pressed("move_down"):
+				move_vec.y = 1 * MOVE_SPEED
+			if Input.is_action_pressed("move_right"):
+				move_vec.x = 1	* MOVE_SPEED
+			elif Input.is_action_pressed("move_left"):
+				move_vec.x = -1	* MOVE_SPEED
+	
