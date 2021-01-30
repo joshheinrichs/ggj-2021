4563f6aaebf3bdcf42b124f2d372336f9af8ff05
telizki1892 <telizki1892@saskpolytech.ca>
2021-01-30 10:52:27 -0600

    Update Player Movement

--- Player.gd
+++ Player.gd
@@ -1,65 +1,73 @@
 extends KinematicBody2D
 
+onready var Overlap = $Overlap_Area
+
 enum{
 	IDLE
 	RUNNING
 	CLIMB_TREE
 }
 
-const UP = Vector2(0,-1)
-const GRAVITY = 20
-const JUMP = -600
-const MOVE_SPEED = 20
-const MAX_SPEED = 0
-const DECCEL = 0.9
-var move_vec = Vector2()
-var leftright = 0
-var treeInRange = false
 var state = RUNNING
 
-onready var Overlap = $Overlap_Area
+const UP = Vector2(0,-1)
+const MAX_SPEED = 350
+const GRAVITY = 25
+const JUMP = -650
 
-func _on_Overlap_Area_area_entered(area):
-	print("It's " + area.name)
-	if area.name == "Tree" and (Input.is_action_just_pressed("move_up") or Input.is_action_just_pressed("move_up")):
-		state = CLIMB_TREE
-		print(state)
-
-func _on_Overlap_Area_area_exited(area):
-	if area.name == "Tree":
-		treeInRange = false
-		#state = RUNNING
+var move_vec = Vector2.ZERO
+var current_area = Vector2.ZERO
 
 func _physics_process(delta):
+	var direction = Vector2.ZERO
+	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	
+	move_vec.y += GRAVITY
+	
 	match state:
 		IDLE:
 			pass
 		RUNNING:
-			if Input.is_action_pressed("move_right") and is_on_floor():
-				move_vec.x += 1	* MOVE_SPEED
-			elif Input.is_action_pressed("move_left") and is_on_floor():
-				move_vec.x -= 1	* MOVE_SPEED
+			if direction != Vector2.ZERO:
+				move_vec.x = lerp(0, direction.x * MAX_SPEED, 0.7)	
 			else:
-				if is_on_floor():
-					move_vec.x *= DECCEL
+				move_vec.x = lerp(move_vec.x, 0, 0.2)
 			
-			#move_vec = move_vec.normalized() * MOVE_SPEED 
-			if treeInRange == false:
-				move_vec.y += GRAVITY
-			
-			move_vec = move_and_slide(move_vec, UP)
-			
-			if is_on_floor() and Input.is_action_just_pressed("jump"):
-				move_vec.y = JUMP
-
+			if is_on_floor():
+				if Input.is_action_just_pressed("jump"):
+					move_vec.y = JUMP
+			else:
+				move_vec.x = lerp(move_vec.x, 0, 0.05)
+				
 		CLIMB_TREE:
-			move_vec = Vector2.ZERO
-			if Input.is_action_pressed("move_up"):
-				move_vec.y = -1 * MOVE_SPEED
-			if Input.is_action_pressed("move_down"):
-				move_vec.y = 1 * MOVE_SPEED
-			if Input.is_action_pressed("move_right"):
-				move_vec.x = 1	* MOVE_SPEED
-			elif Input.is_action_pressed("move_left"):
-				move_vec.x = -1	* MOVE_SPEED
-	
+			move_vec.x = 0
+			if direction != Vector2.ZERO:
+				move_vec.y = lerp(0, direction.y * MAX_SPEED, 0.7)
+			else:
+				move_vec.y = lerp(move_vec.y, 0, 1)
+			
+			if !is_on_floor():
+				if Input.is_action_just_pressed("jump"):
+					move_vec.y = JUMP
+					state = RUNNING
+			
+			if Input.is_action_just_pressed("move_right"):
+				position.x = position.x + current_area.x
+			if Input.is_action_just_pressed("move_left"):
+				position.x = position.x - current_area.x
+				
+	move_vec = move_and_slide(move_vec, UP)
+			
+func _on_Overlap_Area_area_entered(area):
+	print("It's " + area.name)
+	if Input.is_action_pressed("move_up"):
+		state = CLIMB_TREE
+		current_area = area.get_child(0).shape.extents
+		position.x = area.position.x
+		
+
+func _on_Overlap_Area_area_exited(area):
+	state = RUNNING
+
+
