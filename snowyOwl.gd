5c83722d115fc5b629f1d91c5a60dd6eb4fd8b48
HelmutShaftshwingen <alexander.n.heilmann@gmail.com>
2021-01-30 17:11:47 -0600

    Owl update

--- snowyOwl.gd
+++ snowyOwl.gd
@@ -5,13 +5,18 @@ enum {
 	ATTACK
 	PATROL	
 }
-
-const MAX_SPEED = 100
+const UP = Vector2(0,-1)
+const SPEED = 200
+const MAX_SPEED = 4
 const RANGE = 400
 
 var state = WAIT
 var sightAngle = 0
 var rayPoints = [Vector2.ZERO,Vector2.ZERO]
+var playerFound = false
+var prey = null
+var preyVec = Vector2.ZERO
+
 
 func _ready():
 	var rayPoints = [get_node("RayCast2D").position,get_node("RayCast2D").cast_to]
@@ -19,13 +24,25 @@ func _ready():
 func _process(delta):
 	match state:
 		WAIT:
+			get_node("Line2D").visible = true
+			get_node("RayCast2D").enabled = true
 			sightAngle += deg2rad(1)
 			get_node("RayCast2D").cast_to = Vector2(cos(sightAngle)*RANGE,sin(sightAngle)*RANGE)
 			#match the red line to the ray trace
 			rayPoints = [get_node("RayCast2D").position,get_node("RayCast2D").cast_to]
 			get_node("Line2D").points = rayPoints
-			
+			if get_node("RayCast2D").is_colliding() == true:
+				if get_node("RayCast2D").get_collider().name == "Player":
+					prey = get_node("RayCast2D").get_collider()
+					state = ATTACK
+					
 		ATTACK:
-			pass
+			get_node("Line2D").visible = false
+			get_node("RayCast2D").enabled = false
+			preyVec = (prey.position-self.position).normalized() * SPEED
+			preyVec += move_and_slide(preyVec, UP)
 		PATROL:
 			pass
+
+
+
