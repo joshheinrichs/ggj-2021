747939c91e738f94a8844a2b25dd92007ff99024
telizki1892 <telizki1892@saskpolytech.ca>
2021-01-31 11:10:44 -0600

    Movement/Jump_Animation

--- Player.gd
+++ Player.gd
@@ -20,7 +20,7 @@ const JUMP = -650
 const GLIDE = 50
 
 var move_vec = Vector2.ZERO
-var current_area
+var tree_shape
 export var score = 0
 var treeInRange = false
 var flipped_sprite = false
@@ -38,24 +38,20 @@ func _physics_process(delta):
 		if flipped_sprite == false:
 			rotate_sprite()
 		move_vec.x = 0
-		
-		if self.position.x < current_area.position.x:
-			self.position.x = current_area.position.x - (current_area.get_child(0).shape.extents.x)
-		if self.position.x > current_area.position.x:
-			self.position.x = current_area.position.x + (current_area.get_child(0).shape.extents.x)
-		#position.x = current_area.position.x
+		check_tree_side()
 		state = CLIMB_TREE
 
 	match state:
 		IDLE:
 			pass
 		RUNNING:
+			animatedSprite.flip_v = false
+			animatedSprite.flip_h = move_vec.x < 0
 			
-			move_vec.x -= (move_vec.x * DECEL * delta)		
+			move_vec.x -= (move_vec.x * DECEL * delta)
+					
 			if direction != Vector2.ZERO:
-				
 				animatedSprite.play("run")
-				animatedSprite.flip_h = move_vec.x < 0
 				
 				if move_vec.x > MAX_SPEED:
 					move_vec.x = MAX_SPEED
@@ -67,10 +63,12 @@ func _physics_process(delta):
 				animatedSprite.play("idle")
 				
 			if is_on_floor() and Input.is_action_just_pressed("jump"):
+				animatedSprite.play("glide")
 				move_vec.y = JUMP
 			
-			if Input.is_action_pressed("jump") and move_vec.y > GLIDE:
-				move_vec.y = GLIDE
+#			if Input.is_action_pressed("jump") and move_vec.y > GLIDE:
+#				animatedSprite.play("glide")
+#				move_vec.y = GLIDE
 				
 		CLIMB_TREE:
 
@@ -78,7 +76,7 @@ func _physics_process(delta):
 				animatedSprite.play("run")
 				animatedSprite.flip_h = move_vec.y < 0
 				
-				if self.position.x < current_area.position.x:
+				if self.position.x < tree_shape.position.x:
 					animatedSprite.flip_v = true
 				else:
 					animatedSprite.flip_v = false
@@ -88,37 +86,29 @@ func _physics_process(delta):
 				animatedSprite.play("idle")
 				move_vec.y = 0
 
-			if !is_on_floor():
-				if Input.is_action_just_pressed("jump"):
-					animatedSprite.flip_v = false
-					move_vec.y = JUMP
-					move_vec.x = lerp(0, MAX_SPEED * direction.x, 1)
-					state = RUNNING
-			
-#			if Input.is_action_just_pressed("move_right"):
-#				position.x = position.x + current_area.get_child(0).shape.extents.x
-#			if Input.is_action_just_pressed("move_left"):
-#				position.x = position.x - current_area.get_child(0).shape.extents.x
-				
+			if Input.is_action_just_pressed("jump"):
+				animatedSprite.play("glide")
+				move_vec.y = JUMP
+				move_vec.x = MAX_SPEED * direction.x
+				state = RUNNING
+						
 	move_vec = move_and_slide(move_vec, UP)
 			
 func _on_Overlap_Area_area_entered(area):
 	if area.is_in_group("Bugs"):
 		emit_signal("found_bug")
-		# TODO: is there a better way to remove food?
 		area.queue_free()
+		
 	elif area.name == "Tree" or area.get_parent().name == "Trees":
 		treeInRange = true
-		current_area = area
-		# holding up when you enter the tree
-#		if Input.is_action_pressed("move_up"):
-#			state = CLIMB_TREE
+		tree_shape = area
 
 func _on_Overlap_Area_area_exited(area):
+	treeInRange = false
 	if flipped_sprite == true:
 		rotate_sprite()
-	treeInRange = false
 	state = RUNNING
+	
 
 func rotate_sprite():
 	if flipped_sprite == false:
@@ -126,7 +116,13 @@ func rotate_sprite():
 		flipped_sprite = true
 	else:
 		animatedSprite.rotate(deg2rad(-90))
-		flipped_sprite = false		
+		flipped_sprite = false
 
+func check_tree_side():		
+	if self.position.x < tree_shape.position.x:
+		self.position.x = tree_shape.position.x - (tree_shape.get_child(0).shape.extents.x)
+		
+	if self.position.x > tree_shape.position.x:
+		self.position.x = tree_shape.position.x + (tree_shape.get_child(0).shape.extents.x)
 
 
