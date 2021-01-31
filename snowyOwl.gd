a7ab161e74a907d6c93b8b2c459cfa2d715304f6
Josh Heinrichs <joshiheinrichs@gmail.com>
2021-01-30 22:41:53 -0600

    Maybe recover Alex's owl work

--- snowyOwl.gd
+++ snowyOwl.gd
@@ -1,20 +1,19 @@
-@@ -3,12 +3,13 @@ extends KinematicBody2D
+extends KinematicBody2D
 enum {
 	WAIT
 	ATTACK
-	PATROL	
 	PATROL
 	MOVE
 }
 const UP = Vector2(0,-1)
 const SPEED = 200
 const MAX_SPEED = 4
-const RANGE = 400
 const RANGE = 100
 
 var state = WAIT
 var sightAngle = 0
-@ -17,19 +18,23 @@ var playerFound = false
+var rayPoints = [Vector2.ZERO,Vector2.ZERO]
+var playerFound = false
 var prey = null
 var preyVec = Vector2.ZERO
 var move = false
@@ -25,47 +24,39 @@ func _on_Timer_timeout():
 	print("triggered")
 	move = true
 
-	
 func _ready():
 	var rayPoints = [get_node("RayCast2D").position,get_node("RayCast2D").cast_to]
 	get_node("waitTimer").start(3)
-	
-	
+
 func _process(delta):
 	match state:
 		WAIT:
-			#start the wait timer
-			get_node("waitTimer").set_wait_time(10)
-			get_node("waitTimer").start()
-			print($waitTimer.time_left)
 			#start ray and beam
 			get_node("Line2D").visible = true
 			get_node("RayCast2D").enabled = true
-@ -45,8 +50,13 @@ func _process(delta):
+			#rotation of ray
+			sightAngle += deg2rad(1)
+			get_node("RayCast2D").cast_to = Vector2(cos(sightAngle)*RANGE,sin(sightAngle)*RANGE)
+			#match the red line to the ray trace
+			rayPoints = [get_node("RayCast2D").position,get_node("RayCast2D").cast_to]
+			get_node("Line2D").points = rayPoints
+			if get_node("RayCast2D").is_colliding() == true:
+					if get_node("RayCast2D").get_collider().name == "Player":
+							prey = get_node("RayCast2D").get_collider()
 					state = ATTACK
 			if move == true:
 				state = PATROL
-				move == false
-			print(get_node("waitTimer").time_left)
 				move = false
 				var owlBranches = get_tree().get_nodes_in_group("owlBranchGroup")
 				owlBranches.shuffle()
 				targetBranch = owlBranches[0]
 				targetVec = (targetBranch.position-self.position).normalized() * SPEED
-				
-			
 		ATTACK:
 			get_node("Line2D").visible = false
 			get_node("RayCast2D").enabled = false
-@ -54,11 +64,17 @@ func _process(delta):
+			preyVec = (prey.position-self.position).normalized() * SPEED
 			preyVec += move_and_slide(preyVec, UP)
 		PATROL:
-			#find banches
-			var owlBranches = get_tree().get_nodes_in_group("owlBranch")
-			owlBranches.shuffle()
-			var targetBranch = owlBranches[0]
-			var targetVec = (targetBranch.position-self.position).normalized() * SPEED
-			targetVec += move_and_slide(targetVec,UP)
 			move_and_slide(targetVec,UP)
 			for thing in $Area2D.get_overlapping_areas():
 				if thing != null:
@@ -74,8 +65,3 @@ func _process(delta):
 						#self.Position = thing.Position
 						state = WAIT
 						$waitTimer.start(3)
-						
-
-
-
-
