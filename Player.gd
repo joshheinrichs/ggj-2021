1595f54b83b22574bc3493940a73de40bbda3206
Josh Heinrichs <joshiheinrichs@gmail.com>
2021-01-30 19:51:41 -0600

    Tweak movement
    Also add check for bug and tree groups

--- Player.gd
+++ Player.gd
@@ -12,9 +12,11 @@ var state = RUNNING
 
 const UP = Vector2(0,-1)
 const MAX_SPEED = 350
-const ACCEL = 500
+const ACCEL = 3000
+const DECEL = 10
 const GRAVITY = 25
 const JUMP = -650
+const GLIDE = 50
 
 var move_vec = Vector2.ZERO
 var current_area
@@ -39,22 +41,21 @@ func _physics_process(delta):
 		IDLE:
 			pass
 		RUNNING:
-			if direction != Vector2.ZERO:
-				if abs(move_vec.x) < MAX_SPEED:
-					move_vec.x += (direction.x * ACCEL * delta)
-				else: 
-					move_vec.x = direction.x * MAX_SPEED
-				
-				print (move_vec)
+			move_vec.x -= (move_vec.x * DECEL * delta)
 
-			else:
-				move_vec.x = 0
+			if direction != Vector2.ZERO:
+				if move_vec.x > MAX_SPEED:
+					move_vec.x = MAX_SPEED
+				elif move_vec.x < -MAX_SPEED:
+					move_vec.x = -MAX_SPEED
+				else:
+					move_vec.x += (direction.x * ACCEL * delta)
 			
-			if is_on_floor():
-				if Input.is_action_just_pressed("jump"):
-					move_vec.y = JUMP
-			else:
-				move_vec.x = lerp(move_vec.x, 0, 0.05)
+			if is_on_floor() and Input.is_action_just_pressed("jump"):
+				move_vec.y = JUMP
+			
+			if Input.is_action_pressed("jump") and move_vec.y > GLIDE:
+				move_vec.y = GLIDE
 				
 		CLIMB_TREE:
 
@@ -62,7 +63,7 @@ func _physics_process(delta):
 				move_vec.y = direction.y * MAX_SPEED
 			else:
 				move_vec.y = 0
-			
+
 			if !is_on_floor():
 				if Input.is_action_just_pressed("jump"):
 					move_vec.y = JUMP
@@ -77,12 +78,16 @@ func _physics_process(delta):
 	move_vec = move_and_slide(move_vec, UP)
 			
 func _on_Overlap_Area_area_entered(area):
-	if area.name == "Bug":
+	if area.name == "Bug" or area.get_parent().name == "Bugs":
 		emit_signal("found_bug")
-	if area.name == "Tree":
+		# TODO: is there a better way to remove food?
+		area.get_parent().remove_child(area)
+	elif area.name == "Tree" or area.get_parent().name == "Trees":
 		treeInRange = true
 		current_area = area
-		
+		# holding up when you enter the tree
+		if Input.is_action_pressed("move_up"):
+			state = CLIMB_TREE
 
 func _on_Overlap_Area_area_exited(area):
 	treeInRange = false
